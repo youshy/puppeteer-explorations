00e1ab630c81fa33801dffb5f41d0e1659c64457
youshy <devilyoushy@gmail.com>
2019-11-21 11:12:38 +0000

    Iterating over nodes

--- examples/screenshot/main.js
+++ examples/screenshot/main.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 
-const url = "https://ecs.co.uk"
+// const url = "https://ecs.co.uk"
 // const url = "https://bbc.co.uk"
+const url = "https://angular.realworld.io/"
 
 async function getScreenshot() {
   const browser = await puppeteer.launch();
@@ -11,7 +12,18 @@ async function getScreenshot() {
   });
   // await page.listeners(event) -might be useful for finding the listeners
   // await page.eventNames() -will that return the listeners?
-  await page.screenshot({path: 'example.png', fullPage: true});
+  // await page.screenshot({path: 'example.png', fullPage: true});
+
+  // let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+  // console.log(bodyHTML);
+
+  // const listeners = page.eventNames();
+  // console.log(listeners);
+
+  await page.waitForSelector('a');
+
+  const anchors = await page.evaluate(() => [...document.querySelectorAll('a')].map(elem => elem.innerText));
+  console.log(anchors)
 
   console.log("Screenshot taken");
   await browser.close();
